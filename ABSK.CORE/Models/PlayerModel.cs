3f9ddac6d805930dac611c610e1e71a9bec813e5
mattiaerre <mattia.richetto@gmail.com>
2014-03-02 00:15:58 +0000

    Update PlayerModel.cs
    Use interfaces instead

--- ABSK.CORE/Models/PlayerModel.cs
+++ ABSK.CORE/Models/PlayerModel.cs
@@ -7,7 +7,7 @@ namespace ABSK.CORE.Models
   public class PlayerModel
   {
     public string Name { get; set; }
-    public IEnumerable<Frame> Frames { get; set; }
-    public LastFrame LastFrame { get; set; }
+    public IEnumerable<Frame> Frames { get; set; } // todo: use interface
+    public LastFrame LastFrame { get; set; } // todo: use interface
   }
 }
